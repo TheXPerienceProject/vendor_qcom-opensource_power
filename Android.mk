c8f83e098e54671875fefbb71439b90808a9c7c4
Vinay Verma <vvinay@codeaurora.org>
2018-09-14 11:25:46 +0530

    Reduce hwbinder buffer size for power HAL
    Set hwbinder buffer size to 16KB for 32 bit architecture.
    
    Change-Id: I4272a12853269dd6363f58a5bb81caa09cc47e9a
    CRs-Fixed: 2315283
    adapt to our logic
    Signed-off-by: klozz <klozz@vivaldi.net>

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libhwbinder libutils android.hardware.power@1.2
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c service.cpp Power.cpp
 LOCAL_C_INCLUDES := external/libxml2/include \
@@ -121,6 +121,9 @@ ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
 endif
 
 ifneq ($(TARGET_USES_NON_LEGACY_POWERHAL), true)
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DARCH_ARM_32
+endif
 LOCAL_MODULE := power.qcom
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
@@ -130,6 +133,9 @@ else
 
 LOCAL_MODULE := android.hardware.power@1.2-service
 LOCAL_INIT_RC := android.hardware.power@1.2-service.rc
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DARCH_ARM_32
+endif
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_EXECUTABLE)
